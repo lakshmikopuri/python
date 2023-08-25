018dbbd9096324a41e8875c18dac25b24b5dabcc
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-08-25 15:45:13 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,26 @@
+#while loop
+x=1
+while x<=10:
+    print(x)
+    x+=1
+#the sum of 1st n num 
+n=int(input())
+sum=0
+#while loop
+i=1
+while i<=n:
+    sum+=i
+    i+=1
+print(sum)
+name=""
+psd=""
+while name!="lucky": and psw!="python":
+    name=input("enter name:")
+    name=input("enter psw wrd")
+print("hello lucky thamks for confirmation")
+
+#infinite loop
+i=0
+while True:
+    i=i+1
+    print("hello:")
