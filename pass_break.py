aba336b1a4ab4277d1db20f15e8b197791b203bc
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-08-25 15:48:34 +0530

    Add files via upload

---
+++ pass_break.py
@@ -0,0 +1,19 @@
+#break
+for i in range(1,9):
+    if(i==5):
+        print("break")
+        break
+    else:
+        print(i)
+#pass
+for i in range(1,9):
+    if(i==5):
+        print("pass")
+        pass
+    else:
+        print(i)
+l="lucky"
+print(l[0])
+print(l[::])
+print(l[::-1])
+    
