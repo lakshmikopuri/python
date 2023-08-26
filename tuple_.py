e761967428acefaafb2c03a333b7a1f23cad5e2f
lakshmikopuri <141564073+lakshmikopuri@users.noreply.github.com>
2023-08-26 12:44:16 +0530

    Add files via upload

---
+++ tuple_.py
@@ -0,0 +1,15 @@
+t = ("apple", "banana", "cherry")
+print(t[1])
+#Negative Indexing
+t = ("apple", "banana", "cherry")
+print(t[-1])
+#range of indedxing
+t=(1,2,3,4,5,6,7,8)
+print(t[2:6])
+#-ve range Indexing
+t=(1,2,3,4,5,6,7,8)
+print(t[-4:-1])
+#iteration
+t=(1,2,3,4,5,6,7,8)
+for i in t:
+    print(i)
